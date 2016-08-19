9ad9b7b748941a059bfbf40d470cbb24104f434e
J.D. Fagan <jd.fagan@gmail.com>
2016-08-19 10:18:59 -0700

    Implemented super_balanced within binary tree via recursion.  Iterative approach was possible so should have tried that first instead of trying recursion first.

---
+++ InterviewCake/super_balanced_tree.py
@@ -0,0 +1,21 @@
+from InterviewCake.binary_tree import *
+
+
+# Recursive depth first implementation
+def get_depth(node: BinaryTreeNode, aggregate):
+    if node is None:
+        return 0
+
+    if node.left is None and node.right is None:            # leaf
+        return 1
+    elif node.left is not None and node.right is None:      # right leaf
+        return aggregate(get_depth(node.left, aggregate), 0) + 1
+    elif node.left is None and node.right is not None:      # left leaf
+        return aggregate(0, get_depth(node.right, aggregate)) + 1
+    else:
+        return aggregate(get_depth(node.left, aggregate), get_depth(node.right, aggregate)) + 1
+
+
+# O(2n) time and O(n) space (due to recursive stack)
+def is_super_balanced(node: BinaryTreeNode):
+    return get_depth(node, max) - get_depth(node, min) <= 1
---
+++ InterviewCake/test/test_super_balanced_tree.py
@@ -0,0 +1,81 @@
+from InterviewCake.binary_tree import *
+from InterviewCake.super_balanced_tree import *
+
+def test_super_balanced_tree1():
+    a = BinaryTreeNode(1)
+    b = BinaryTreeNode(2)
+    c = BinaryTreeNode(3)
+    a.left = b
+    a.right = c
+    assert is_super_balanced(a)
+
+
+def test_super_balanced_tree2():
+    a = BinaryTreeNode(1)
+    b = BinaryTreeNode(2)
+    c = BinaryTreeNode(3)
+    d = BinaryTreeNode(4)
+    a.left = b
+    a.right = c
+    b.left = d
+    assert is_super_balanced(a)
+
+
+def test_super_balanced_tree3():
+    a = BinaryTreeNode(1)
+    b = BinaryTreeNode(2)
+    c = BinaryTreeNode(3)
+    d = BinaryTreeNode(4)
+    e = BinaryTreeNode(5)
+    a.left = b
+    a.right = c
+    b.left = d
+    c.left = e
+    assert is_super_balanced(a)
+
+
+def test_super_balanced_tree4():
+    a = BinaryTreeNode(1)
+    b = BinaryTreeNode(2)
+    c = BinaryTreeNode(3)
+    d = BinaryTreeNode(4)
+    e = BinaryTreeNode(5)
+    f = BinaryTreeNode(6)
+    a.left = b
+    a.right = c
+    b.left = d
+    c.left = e
+    d.right = f
+    assert not is_super_balanced(a)
+
+
+def test_list():
+    a = BinaryTreeNode(1)
+    b = BinaryTreeNode(2)
+    c = BinaryTreeNode(3)
+    a.right = b
+    b.right = c
+    assert not is_super_balanced(a)
+
+
+def test_left_leaf():
+    a = BinaryTreeNode(1)
+    b = BinaryTreeNode(2)
+    a.right = b
+    assert is_super_balanced(a)
+
+
+def test_right_leaf():
+    a = BinaryTreeNode(1)
+    b = BinaryTreeNode(2)
+    a.left = b
+    assert is_super_balanced(a)
+
+
+def test_one_elements():
+    a = BinaryTreeNode(1)
+    assert is_super_balanced(a)
+
+
+def test_no_elements():
+    assert is_super_balanced(None)
