404e27ff117faa5b5722b428a9e6941ac9243351
J.D. Fagan <jd.fagan@gmail.com>
2016-08-18 19:39:32 -0700

    Optimal solution for coin combo is done via bottom up.  Avoids recursion and thus extra memory space of stack.  Done in O(n*m) time with O(n) space.

--- InterviewCake/coin_combos.py
+++ InterviewCake/coin_combos.py
@@ -17,37 +17,52 @@ def coin_combos(amount, coins):
 
 
 def get_coin_combos(amount, coins):
-    return change_possibilities_top_down(amount, coins)
+    return change_possibilities_bottom_up(amount, coins)
 
 
-def change_possibilities_top_down(amount_left, denominations_left):
+# O(n*m) time and O(n) space
+def change_possibilities_bottom_up(amount, coins):
+    ways_of_n = [0] * (amount + 1)
+    ways_of_n[0] = 1
+
+    for coin in coins:
+        for higher_amount in range(coin, amount + 1):
+            higher_amount_remainder = higher_amount - coin
+            ways_of_n[higher_amount] += ways_of_n[higher_amount_remainder]
+            print(str.format("checking ways to make {} with {}", higher_amount, coin))
+
+    return ways_of_n[amount]
+
+
+# O(n*m) time and O(n*m) space - extra m space due to call stack of recursion
+def change_possibilities_top_down(amount, coins):
 
     # base cases:
     # we hit the amount spot on. yes!
-    if amount_left == 0:
+    if amount == 0:
         return 1
     # we overshot the amount left (used too many coins)
-    if amount_left < 0:
+    if amount < 0:
         return 0
     # we're out of denominations
-    if len(denominations_left) == 0:
+    if len(coins) == 0:
         return 0
 
-    print(str.format("checking ways to make {} with {}", amount_left, denominations_left))
+    print(str.format("checking ways to make {} with {}", amount, coins))
 
     # choose a current coin
-    current_coin, rest_of_coins = denominations_left[0], denominations_left[1:]
+    current_coin, rest_of_coins = coins[0], coins[1:]
 
     # see how many possibilities we can get
     # for each number of times to use current_coin
     possibilities = {}
     num_possibilities = 0
-    while amount_left >= 0:
-        if possibilities.get(amount_left) is None:
-            possibilities[amount_left] = change_possibilities_top_down(amount_left, rest_of_coins)
-        num_possibilities += possibilities[amount_left]
+    while amount >= 0:
+        if possibilities.get(amount) is None:
+            possibilities[amount] = change_possibilities_top_down(amount, rest_of_coins)
+        num_possibilities += possibilities[amount]
         # num_possibilities += change_possibilities_top_down(amount_left, rest_of_coins)
-        amount_left -= current_coin
+        amount -= current_coin
         # print(str.format("({}): checking ways to make {} with {}", num_possibilities, amount_left, denominations_left))
 
     return num_possibilities
