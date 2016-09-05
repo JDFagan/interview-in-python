6768bf6e20ca9d8065cfbe89ce45f7a7193cd935
J.D. Fagan <jd.fagan@gmail.com>
2016-09-04 20:37:12 -0700

    Fixed flaw with BST checker by making sure to look at largest left and right side deeply when comparing to parent node

--- InterviewCake/bst_checker.py
+++ InterviewCake/bst_checker.py
@@ -1,6 +1,12 @@
 from InterviewCake.binary_tree import *
 
 
+def largest(node):
+    while node.right:
+        node = node.right
+    return node
+
+
 # O(n) time and O(h) space where h is height of tree
 def is_binary_search_tree(node):
     if node is None:
@@ -8,9 +14,12 @@ def is_binary_search_tree(node):
     if node.left is None:
         if node.right is None:
             return True
-        return node.value < node.right.value and is_binary_search_tree(node.right)
+        return node.value < node.right.value and node.value < largest(node.right).value \
+                and is_binary_search_tree(node.right)
     elif node.right is None:
-        return node.value > node.left.value and is_binary_search_tree(node.left)
+        return node.value > node.left.value and node.value > largest(node.left).value \
+                and is_binary_search_tree(node.left)
 
     return node.left.value < node.value < node.right.value \
+            and largest(node.left).value < node.value < largest(node.right).value \
             and is_binary_search_tree(node.left) and is_binary_search_tree(node.right)
