65cfcdd677a3e9186085531f6b27dbe4b18eae35
J.D. Fagan <jd.fagan@gmail.com>
2016-09-05 16:24:25 -0700

    Smarter recursive solution that takes advantage of cached memoization tactics so computation time is done in O(n) time and O(1) space.

--- InterviewCake/fibonacci.py
+++ InterviewCake/fibonacci.py
@@ -1,4 +1,4 @@
-# Not great recursive solution: O(2^n) time and O(2^n) space
+# Naive recursive solution: O(2^n) time and O(2^n) space
 def fib_recursive(n):
     if n < 0:
         raise IndexError("No such valid Fibonacci value for negative number")
@@ -7,3 +7,33 @@ def fib_recursive(n):
 
     print("computing fib_recursive({})".format(n))
     return fib_recursive(n - 2) + fib_recursive(n - 1)
+
+
+# Smart recursive solution that uses memoization to cache expensive calculations bottom up
+# O(n) time and O(1) space
+class Fibonacci:
+    def __init__(self):
+        self.memo = {}
+
+    def fib(self, n):
+        # Edge case
+        if n < 0:
+            raise IndexError("No such valid Fibonacci value for negative number")
+
+        # Base cases
+        if n in [0, 1]:
+            return n
+
+        # See if we've already calculated this before
+        if n in self.memo:
+            print("Grabbing memo[{}]".format(n))
+            return self.memo[n]
+
+        # Compute it
+        print("Computing fib({})".format(n))
+        result = self.fib(n - 1) + self.fib(n - 2)
+
+        # Memoize
+        self.memo[n] = result
+
+        return result
--- InterviewCake/test/test_fibonacci.py
+++ InterviewCake/test/test_fibonacci.py
@@ -2,6 +2,21 @@ from InterviewCake.fibonacci import *
 import pytest
 
 
+def test_fib_memoization():
+    fibber = Fibonacci()
+    assert fibber.fib(8) == 21
+    with pytest.raises(IndexError):
+        assert fibber.fib(-1)
+    assert fibber.fib(0) == 0
+    assert fibber.fib(1) == 1
+    assert fibber.fib(2) == 1
+    assert fibber.fib(3) == 2
+    assert fibber.fib(4) == 3
+    assert fibber.fib(5) == 5
+    assert fibber.fib(6) == 8
+    assert fibber.fib(7) == 13
+
+
 def test_fib_error():
     n = -1
     with pytest.raises(IndexError):
