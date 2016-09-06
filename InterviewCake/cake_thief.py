9a0d6226b1e91559be9f0c368dd32dcc67a188a4
J.D. Fagan <jd.fagan@gmail.com>
2016-09-05 19:51:06 -0700

    Tried to add additional logic to handle some odd edge cases (e.g., 0 capacity duffel bag, 0 value or 0 weight cakes).
    Not deemed most optimal answer but my answer actually considered good as its more efficient than optimal answer according to InterviewCake website.

--- InterviewCake/cake_thief.py
+++ InterviewCake/cake_thief.py
@@ -1,20 +1,28 @@
 import bisect
+import math
 
 # Tactic is to first order by normalized values by one unit of weight and fill duffel bag first by this ordering.
+# Note: my solution is faster algorithm that gives us a good answer, even if it's not the optimal answer.
+# Sometimes an efficient, good answer might be more practical than an inefficient, optimal answer.
 # O(n) time and O(n) space
 def max_duffel_bag_value(cake_tuples, capacity):
     cakes_by_value_per_weight = []
     for i in range(len(cake_tuples)):
         cake_tuple = cake_tuples[i]
-        bisect.insort(cakes_by_value_per_weight, (cake_tuple[1]/cake_tuple[0], i))
+        cake_weight = cake_tuple[0]
+        cake_value = cake_tuple[1]
+        cake_value_per_weight = cake_value/cake_weight if cake_weight > 0 else math.inf*cake_value
+        bisect.insort(cakes_by_value_per_weight, (cake_value_per_weight, i))
 
     result = 0
     for i in reversed(range(len(cakes_by_value_per_weight))):
         cake_index = cakes_by_value_per_weight[i][1]
         cake_weight = cake_tuples[cake_index][0]
         cake_value = cake_tuples[cake_index][1]
-        cakes = capacity//cake_weight
-        result += cakes*cake_value
+        cakes = capacity//cake_weight if cake_weight > 0 else math.inf
+        result += cakes*cake_value if cake_value > 0 else 0
+        if result >= math.inf:
+            break
         capacity -= cakes*cake_weight
 
     return result
--- InterviewCake/test/test_cake_thief.py
+++ InterviewCake/test/test_cake_thief.py
@@ -1,10 +1,29 @@
 from InterviewCake.cake_thief import *
+import math
 
 
 def test_no_elements():
     cake_tuples = []
     capacity = 20
     assert max_duffel_bag_value(cake_tuples, capacity) == 0
+
+
+def test_zero_capacity():
+    cake_tuples = [(7, 160), (3, 90), (2, 15)]
+    capacity = 0
+    assert max_duffel_bag_value(cake_tuples, capacity) == 0
+
+
+def test_zero_cake_weight():
+    cake_tuples = [(7, 160), (0, 90), (2, 15)]
+    capacity = 20
+    assert max_duffel_bag_value(cake_tuples, capacity) == math.inf
+
+
+def test_zero_cake():
+    cake_tuples = [(7, 160), (0, 0), (2, 15)]
+    capacity = 20
+    assert max_duffel_bag_value(cake_tuples, capacity) == math.nan
 
 
 def test_thief1():
