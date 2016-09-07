027a56068ee4dd81c3933d868b2f4556cc514376
J.D. Fagan <jd.fagan@gmail.com>
2016-09-07 00:17:26 -0700

    Better solution in O(1) time and space!

--- InterviewCake/delete_linked_list.py
+++ InterviewCake/delete_linked_list.py
@@ -1,20 +1,14 @@
 from InterviewCake.linked_list import *
 
 
-# O(n) time and O(1) space
+# O(1) time and O(1) space!
 class LinkedList:
     def __init__(self, root: LinkedListNode):
         self.root = root
 
     def delete_node(self, node: LinkedListNode):
-        prev = current = self.root
-
-        while current:
-            if current == node:
-                if prev == self.root:       # edge root case
-                    self.root = node.next
-                prev.next = node.next
-                break
-            else:
-                prev = current
-                current = current.next
+        if node.next:
+            node.value = node.next.value
+            node.next = node.next.next
+        else:
+            node = node.next
