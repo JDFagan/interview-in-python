7591ad37f6c4219d3ae214b8a5bc4bb3b1cc1b36
J.D. Fagan <jd.fagan@gmail.com>
2016-09-07 00:24:54 -0700

    Added commentary about the risk to this O(1) time solution due to side effects of editing in place.

--- InterviewCake/delete_linked_list.py
+++ InterviewCake/delete_linked_list.py
@@ -2,6 +2,9 @@ from InterviewCake.linked_list import *
 
 
 # O(1) time and O(1) space!
+# Note, this solution is risky instead of walking from root to where we need to delete.  The reason why
+# is because we are editing the linked list in place and leaving some dangling nodes.  If there are other
+# pointers to these nodes that got left off due to this algorithm, it could incur subtle side effect bugs!
 class LinkedList:
     def __init__(self, root: LinkedListNode):
         self.root = root
