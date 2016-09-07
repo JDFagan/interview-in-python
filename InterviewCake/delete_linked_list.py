3d13a3ab62141c1ef2215640703b4b9ba7d13f9c
J.D. Fagan <jd.fagan@gmail.com>
2016-09-07 00:04:52 -0700

    My solution to deleting node of linked list.  Done in O(n) time and O(1) space.

--- InterviewCake/delete_linked_list.py
+++ InterviewCake/delete_linked_list.py
@@ -1,4 +1,20 @@
-class LinkedListNode:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+from InterviewCake.linked_list import *
+
+
+# O(n) time and O(1) space
+class LinkedList:
+    def __init__(self, root: LinkedListNode):
+        self.root = root
+
+    def delete_node(self, node: LinkedListNode):
+        prev = current = self.root
+
+        while current:
+            if current == node:
+                if prev == self.root:       # edge root case
+                    self.root = node.next
+                prev.next = node.next
+                break
+            else:
+                prev = current
+                current = current.next
