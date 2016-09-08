536e7ce13b8a199acb3f25ab6a5406fc4e6b08f3
J.D. Fagan <jd.fagan@gmail.com>
2016-09-08 14:55:21 -0700

    Slightly more stylish solution - simply track the unpaired letters.  Palindromes can only have 0 or 1 unpaired letters.

--- InterviewCake/string_palindrome.py
+++ InterviewCake/string_palindrome.py
@@ -1,23 +1,11 @@
 # O(n) time and O(u) space where u is unique letters in the input
 def has_palindrome(word: str):
-    letter_is_odd = {}
+    unpaired_letters = set()
 
     for char in word:
-        if char in letter_is_odd:
-            # Toggle odd-ness of existing letter
-            letter_is_odd[char] = not letter_is_odd[char]
+        if char in unpaired_letters:
+            unpaired_letters.remove(char)
         else:
-            # First instance of letter found means its odd count of this letter
-            letter_is_odd[char] = True
+            unpaired_letters.add(char)
 
-    odd_letters = 0
-    word_must_have_odd = not len(word) % 2 == 0
-
-    for odds in letter_is_odd.values():
-        if odds:
-            odd_letters += 1
-
-    if word_must_have_odd:
-        return odd_letters == 1
-    else:
-        return odd_letters == 0
+    return len(unpaired_letters) <= 1
