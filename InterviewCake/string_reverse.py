538d90d76f36956061b55294bd8c3da26bd9e840
J.D. Fagan <jd.fagan@gmail.com>
2016-09-07 20:33:47 -0700

    Bit more style with this version (reads better) - but essentially same solution I gave.

--- InterviewCake/string_reverse.py
+++ InterviewCake/string_reverse.py
@@ -1,16 +1,19 @@
-# O(n/2) time and O(n) space
+# O(n) time and O(n) space
 def reverse_string(s):
     if s is None:
         return None
 
     chars = list(s)
+    left = 0
+    right = len(chars) - 1
 
-    ceiling_index = len(chars)
     # swap in place high indices with low indices
-    for i in range(ceiling_index//2):
-        temp = chars[i]
-        chars[i] = chars[ceiling_index - 1 - i]
-        chars[ceiling_index - 1 - i] = temp
+    while left < right:
+        temp = chars[left]
+        chars[left] = chars[right]
+        chars[right] = temp
+        left += 1
+        right -= 1
 
     # rebuild back the reversed string
     return ''.join(chars)
