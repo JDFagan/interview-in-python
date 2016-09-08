41cb11362fa7d8ed1d62d58327b8636ea9336d2c
J.D. Fagan <jd.fagan@gmail.com>
2016-09-08 13:07:19 -0700

    Better solution that only uses O(1) space.  Don't really need a stack when a simple counter will do.  And no need to get list of chars from input string when one can simply reference within the string for read-only operations.

--- InterviewCake/parenthesis.py
+++ InterviewCake/parenthesis.py
@@ -1,19 +1,18 @@
-# O(n) time and O(n) space
+# O(n) time and O(1) space
 def match(input, parens_position):
     input_len = len(input)
     if parens_position < 0 or parens_position > input_len - 1 or input[parens_position] != "(":
         raise IndexError("Parenthesis position index error - no parens at that location")
 
-    parens_stack = []
-    chars = list(input)
+    parens_open_count = 0
     i = parens_position + 1
     while i < input_len:
-        if chars[i] == "(":
-            parens_stack.append(1)
-        elif chars[i] == ")":
-            if parens_stack == []:
+        if input[i] == "(":
+            parens_open_count += 1
+        elif input[i] == ")":
+            if parens_open_count == 0:
                 return i
-            parens_stack.pop()
+            parens_open_count -= 1
         i += 1
 
     return None
