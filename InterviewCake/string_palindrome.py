1082ecee80681ca1cc855dc1374c1748dcb93e15
J.D. Fagan <jd.fagan@gmail.com>
2016-09-08 16:33:36 -0700

    Additional Big O commentary regarding space complexity for this problem

--- InterviewCake/string_palindrome.py
+++ InterviewCake/string_palindrome.py
@@ -1,4 +1,6 @@
-# O(n) time and O(u) space where u is unique letters in the input
+# O(n) time and O(u) space where u is unique letters in the input.  It's possible to think that the
+# unique letters at worse is a constant number (e.g., only a 100k Unicode characters), so, one could
+# say its actually O(1) space.
 def has_palindrome(word: str):
     unpaired_letters = set()
 
