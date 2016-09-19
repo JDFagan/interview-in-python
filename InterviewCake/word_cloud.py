2cb82e0ebedaef1897f4cd25234358670c53013f
J.D. Fagan <jd.fagan@gmail.com>
2016-09-19 12:57:59 -0700

    My solution to InterviewCake's word cloud problem.  My first cut solution is reasonable solution done in O(n) time and O(n) space.

---
+++ InterviewCake/word_cloud.py
@@ -0,0 +1,35 @@
+# O(n) time and O(n) space
+class WordCloud:
+    # Write code that takes a long string and builds its word cloud data in a dictionary,
+    # where the keys are words and the values are the number of times the words occurred.
+
+    punctuation = {'.', ',', '?', "'", '"', ';', ':', '[', ']', '{', '}', '(', ')', '!', '`'}
+
+    @staticmethod
+    def strip_punctuation(string: str):
+        chars = list(string)
+        stripped_chars = []
+        for c in chars:
+            if c not in WordCloud.punctuation:
+                stripped_chars.append(c)
+
+        return ''.join(stripped_chars)
+
+    def __init__(self, string: str):
+        self.string = self.stripped_string = None
+        self.word_cloud = {}
+        self.set_string(string)
+
+    def set_string(self, string: str):
+        self.string = string
+        self.stripped_string = WordCloud.strip_punctuation(string.lower())
+        self.__calculate()
+
+    def __calculate(self):
+        self.word_cloud.clear()
+        words = self.stripped_string.split()
+        for word in words:
+            if word in self.word_cloud:
+                self.word_cloud[word] += 1
+            else:
+                self.word_cloud[word] = 1
