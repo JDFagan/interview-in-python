255e9b515b6891ad6cd1ab67e40aad7c9306cb72
J.D. Fagan <jd.fagan@gmail.com>
2016-09-18 17:59:02 -0700

    Didn't realize nuance of question that the input is a "ranged" list from 1..n.  With this strict input constraint, one could optimize the space down to O(1) using some mathematical tricks via triangular series.  Modified code shows both generic solution for any integer list of numbers vs. the narrow question posed by InterviewCake of ranged integer list.

--- InterviewCake/find_duplicate.py
+++ InterviewCake/find_duplicate.py
@@ -1,5 +1,5 @@
 # O(n) time and O(n) space
-def find_duplicate_number(numbers: []):
+def find_duplicate_number_not_in_a_range(numbers: []):
     unique_numbers = set()
     for n in numbers:
         if n in unique_numbers:
@@ -7,3 +7,21 @@ def find_duplicate_number(numbers: []):
         unique_numbers.add(n)
 
     return None
+
+
+# O(n) time and O(1) space
+def find_duplicate_number_in_range(number_range: []):
+    if len(number_range) == 0:
+        return 0
+
+    sum_of_list = 0
+    for n in number_range:
+        sum_of_list += n
+
+    n = number_range[len(number_range) - 1]
+    triangular_series_sum = (n**2 + n)/2
+    print("sum_of_list = {}, n = {}, triangular_series_sum = {}, dup_num = {}"
+          .format(sum_of_list, n, triangular_series_sum, triangular_series_sum - sum_of_list))
+
+    # Difference between triangular series and sum of list will be the duplicate number
+    return sum_of_list - triangular_series_sum
--- InterviewCake/test/test_find_duplicate.py
+++ InterviewCake/test/test_find_duplicate.py
@@ -3,24 +3,49 @@ from InterviewCake.find_duplicate import *
 
 def test_no_elements():
     numbers = []
-    assert find_duplicate_number(numbers) is None
+    assert find_duplicate_number_not_in_a_range(numbers) is None
 
 
 def test_one_elements():
     numbers = [1]
-    assert find_duplicate_number(numbers) is None
+    assert find_duplicate_number_not_in_a_range(numbers) is None
 
 
 def test_dup0():
     numbers = [1, 2, 3]
-    assert find_duplicate_number(numbers) is None
+    assert find_duplicate_number_not_in_a_range(numbers) is None
 
 
 def test_dup1():
-    numbers = [1, 2, 3, 4, 3]
-    assert find_duplicate_number(numbers) == 3
+    numbers = [1, 2, 3, 3, 4]
+    assert find_duplicate_number_not_in_a_range(numbers) == 3
 
 
 def test_dup2():
-    numbers = [2, 1, -10, 0, 1]
-    assert find_duplicate_number(numbers) == 1
+    numbers = [1, 1, 2, 3]
+    assert find_duplicate_number_not_in_a_range(numbers) == 1
+
+
+def test_range_no_elements():
+    numbers = []
+    assert find_duplicate_number_in_range(numbers) == 0
+
+
+def test_range_one_elements():
+    numbers = [1]
+    assert find_duplicate_number_in_range(numbers) == 0
+
+
+def test_range_dup0():
+    numbers = [1, 2, 3]
+    assert find_duplicate_number_in_range(numbers) == 0
+
+
+def test_range_dup1():
+    numbers = [1, 2, 3, 3, 4]
+    assert find_duplicate_number_in_range(numbers) == 3
+
+
+def test_range_dup2():
+    numbers = [1, 1, 2, 3]
+    assert find_duplicate_number_in_range(numbers) == 1
