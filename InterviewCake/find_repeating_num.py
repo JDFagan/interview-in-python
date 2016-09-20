7b6fed378f093b322b27aabf2b70867b91f6c60f
J.D. Fagan <jd.fagan@gmail.com>
2016-09-20 15:11:15 -0700

    Refactored solution to optimize time from O(n) time to O(log(n)) time

--- InterviewCake/find_repeating_num.py
+++ InterviewCake/find_repeating_num.py
@@ -1,14 +1,36 @@
-# O(n) time and O(1) space
+# O(log(n)) time and O(1) space
 def find_repeating_num_in_range(nums_range: []):
     # We have a list of integers, where: The integers are in the range 1..n AND The list has a length of n+1
-    # It follows that our list has at least one integer which appears at least twice. But it may have several duplicates,
-    # and each duplicate may appear more than twice.
+    # It follows that our list has at least one integer which appears at least twice.
+    # But it may have several duplicates, and each duplicate may appear more than twice.
     # Write a function which finds any integer that appears more than once in our list.
 
-    prev_num = 0
-    for n in nums_range:
-        if n == prev_num:
-            return n
-        prev_num = n
+    floor_index = -1
+    ceiling_index = len(nums_range)
+    has_duplicate = False
 
+    # if there isn't at least 1 index between floor and ceiling,
+    # we've run out of guesses and the number must not be present
+    while floor_index + 1 < ceiling_index:
+        # find the index ~halfway between the floor and ceiling
+        # we use integer division, so we'll never get a "half index"
+        distance = ceiling_index - floor_index
+        half_distance = distance//2
+        n_index = floor_index + half_distance
+        n_value = nums_range[n_index]
+
+        print("n_index = {}, n_value = {}".format(n_index, n_value))
+
+        if n_value == n_index + 1:
+            # duplicate is to the right so move floor to the right
+            floor_index = n_index
+        else:
+            # a duplicate must exist
+            has_duplicate = True
+            ceiling_index = n_index
+
+    if has_duplicate:
+        return n_value
+
+    # Couldn't find the repeating num
     return None
--- InterviewCake/test/test_find_repeating_num.py
+++ InterviewCake/test/test_find_repeating_num.py
@@ -44,3 +44,28 @@ def test_repeating5():
 def test_repeating6():
     nums = [1, 2, 3, 4, 4]
     assert find_repeating_num_in_range(nums) == 4
+
+
+def test_repeating7():
+    nums = [1, 1, 2, 3, 4, 5, 6, 7]
+    assert find_repeating_num_in_range(nums) == 1
+
+
+def test_repeating8():
+    nums = [1, 2, 3, 3, 4, 5, 6, 7, 8, 9]
+    assert find_repeating_num_in_range(nums) == 3
+
+
+def test_repeating9():
+    nums = [1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert find_repeating_num_in_range(nums) == 3
+
+
+def test_repeating10():
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    assert find_repeating_num_in_range(nums) is None
+
+
+def test_repeating11():
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    assert find_repeating_num_in_range(nums) == 11
