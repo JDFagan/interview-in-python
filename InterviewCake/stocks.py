d129f42f5029babcc3c7e85e4b8cdd76a4cefc44
J.D. Fagan <jd.fagan@gmail.com>
2017-01-03 16:25:59 -0800

    More elegant solution - makes use of enumerate function

--- InterviewCake/stocks.py
+++ InterviewCake/stocks.py
@@ -3,25 +3,16 @@ def get_max_profit(stock_prices_yesterday):
     if len(stock_prices_yesterday) < 2:
         raise IndexError('Getting a profit requires at least 2 prices')
 
-    min_price = max_price = None
-    max_profit = None
+    min_price = stock_prices_yesterday[0]
+    max_profit = stock_prices_yesterday[1] - stock_prices_yesterday[0]
 
     # Greedy solution requiring O(n) time and O(1) space
-    for i in range(len(stock_prices_yesterday)):
+    for i, current_price in enumerate(stock_prices_yesterday):
         if i == 0:
-            min_price = i
             continue
 
-        if max_price is None or stock_prices_yesterday[i] > stock_prices_yesterday[max_price]:
-            max_price = i
-
-        # print("i = " + i)
-        profit = stock_prices_yesterday[max_price] - stock_prices_yesterday[min_price]
-        if max_profit is None or profit > max_profit:
-            max_profit = profit
-
-        if stock_prices_yesterday[i] < stock_prices_yesterday[min_price]:
-            min_price = i
-            max_price = None
+        profit = current_price - min_price
+        max_profit = max(max_profit, profit)
+        min_price = min(min_price, current_price)
 
     return max_profit
