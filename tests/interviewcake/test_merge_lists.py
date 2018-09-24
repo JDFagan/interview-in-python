346bcd0a4435f0a7fda6497806ccca0bb8ab7783
J.D. Fagan <jd.fagan@gmail.com>
2018-09-23 21:57:09 -0700

    Fix merge_lists code and tests.  Now all existing written tests pass.

--- tests/interviewcake/test_merge_lists.py
+++ tests/interviewcake/test_merge_lists.py
@@ -4,52 +4,52 @@ from interviewcake.merge_lists import *
 def test_no_elements():
     list1 = []
     list2 = []
-    assert merge_lists(list1, list2) == []
+    assert [] == merge_lists(list1, list2)
 
 def test_no_elements1():
     list1 = []
     list2 = [1, 2]
-    assert merge_lists(list1, list2) == [1, 2]
+    assert [1, 2] == merge_lists(list1, list2)
 
 
 def test_no_elements2():
     list1 = [1, 9]
     list2 = []
-    assert merge_lists(list1, list2) == [1, 9]
+    assert [1, 9] == merge_lists(list1, list2)
 
 
 def test_merge1():
     list1 = [1, 2, 3]
     list2 = [4, 5, 6]
-    assert merge_lists(list1, list2) == [1, 2, 3, 4, 5, 6]
+    assert [1, 2, 3, 4, 5, 6] == merge_lists(list1, list2)
 
 
 def test_merge2():
     list1 = [1, 3, 5]
     list2 = [2, 4, 6]
-    assert merge_lists(list1, list2) == [1, 2, 3, 4, 5, 6]
+    assert [1, 2, 3, 4, 5, 6] == merge_lists(list1, list2)
 
 
 def test_merge3():
     list1 = [-3, 5, 20]
     list2 = [2, 4, 6, 10]
-    assert merge_lists(list1, list2) == [-3, 2, 4, 5, 6, 10, 20]
+    assert [-3, 2, 4, 5, 6, 10, 20] == merge_lists(list1, list2)
 
 
 def test_merge4():
     list1 = [1, 2, 3]
     list2 = [3, 5, 6]
-    assert merge_lists(list1, list2) == [1, 2, 3, 3, 5, 6]
+    assert [1, 2, 3, 3, 5, 6] == merge_lists(list1, list2)
 
 
 def test_merge5():
     my_list = [3, 4, 6, 10, 11, 15]
     alices_list = [1, 5, 8, 12, 14, 19]
-    assert merge_lists(my_list, alices_list) == [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
+    assert [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19] == merge_lists(my_list, alices_list)
 
 
 def test_merge_many1():
     list1 = [1, 5, 10, 15, 20]
     list2 = [7, 10, 99]
     list3 = [-1, 3, 7, 20, 34]
-    assert merge_many([list1, list2, list3]) == [-1, 1, 3, 5, 7, 7, 10, 10, 15, 20, 20, 34, 99]
+    assert [-1, 1, 3, 5, 7, 7, 10, 10, 15, 20, 20, 34, 99] == merge_many([list1, list2, list3])
