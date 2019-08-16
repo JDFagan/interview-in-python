24ef3637248dad8de9e3da2cc37664879326800b
J.D. Fagan <jd.fagan@gmail.com>
2019-08-16 14:09:14 -0700

    Very close!  Works for most cases and solves the r=1 case.  Takes too long (not efficient) for very large input arrays (100k elements).  Need to refactor solution to possibly not use array within dictionary.

--- src/hackerrank/medium/triplets.py
+++ src/hackerrank/medium/triplets.py
@@ -9,40 +9,27 @@ import sys
 
 # Complete the countTriplets function below.
 def countTriplets(arr, r):
-    # O(n)
+    result = 0
     triplets = {}
-    for an in arr:
-        if an in triplets:
-            triplets[an]['n1_count'] += 1
-        else:
-            triplets[an] = {
-                'n1_count': 1,      # an = a*r**(n-1) = a*r**(1-1) = a*r**(0) = a*1 = a
-                'n2_count': 0,      # an = a*r**(n-1) = a*r**(2-1) = a*r**(1) = a*r; a = an/r
-                'n3_count': 0,      # an = a*r**(n-1) = a*r**(3-1) = a*r**(2) = a*r**2; a = an/(r**2)
-                }
-
+    for i, an in enumerate(arr):
         a2 = an / r
         a3 = an / (r ** 2)
-        if a2 in triplets:
-            triplets[a2]['n2_count'] += 1
-            if a2 == an:
-                if triplets[a2]['n2_count'] == 1:
-                    triplets[an]['n1_count'] -= 1
-                else:
-                    triplets[a2]['n2_count'] -= 1
+
         if a3 in triplets:
-            triplets[a3]['n3_count'] += 1
-            if a3 == an:
-                if triplets[a3]['n3_count'] == 1:
-                    triplets[an]['n1_count'] -= 1
-                else:
-                    triplets[a3]['n3_count'] -= 1
-
-    # O(n)
-    # Visit all potential triplets and count the ones that make up actual geometric series
-    result = 0
-    for a, triplet in triplets.items():
-        result += (triplet['n1_count'] * triplet['n2_count'] * triplet['n3_count'])
-
-    # Big O = O(n) + O(n) = O(2n) = O(n)
+            for a3_map in triplets[a3]:
+                a3_map['n3_count'] += 1
+                result += a3_map['n2_count']
+        if a2 in triplets:
+            for a2_map in triplets[a2]:
+                a2_map['n2_count'] += 1
+        if an in triplets:
+            triplets[an].append({
+                'n2_count': 0,      # an = a*r**(n-1) = a*r**(2-1) = a*r**(1) = a*r; a = an/r
+                'n3_count': 0,      # an = a*r**(n-1) = a*r**(3-1) = a*r**(2) = a*r**2; a = an/(r**2)
+                })
+        else:
+            triplets[an] = [{
+                'n2_count': 0,      # an = a*r**(n-1) = a*r**(2-1) = a*r**(1) = a*r; a = an/r
+                'n3_count': 0,      # an = a*r**(n-1) = a*r**(3-1) = a*r**(2) = a*r**2; a = an/(r**2)
+                }]
     return result
